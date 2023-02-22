b487f2dab058f9d0c74884060673d6719595cb07
Francisco Salar Pérez <frasalper@alu.edu.gva.es>
2023-02-22 10:33:34 +0100

    versión comentada

--- src/Rectangulo_FSP.java
+++ src/Rectangulo_FSP.java
@@ -1,19 +1,36 @@
-
+/**
+ * @author Francisco Salar Pérez
+ * @version 1.2
+ */
 public class Rectangulo_FSP extends FiguraGeometrica_FSP {
 	private double l1;
 	private double l2;
-	
+
+	/**
+	 * Método para recibir los parámetros necesarios
+	 * @param tipoFigura String para comprobar
+	 * @param lG double para comprobar
+	 * @param lP double para comprobar
+	 */
 	public Rectangulo_FSP(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
 	}
 
+	/**
+	 * Método que permite calcular el área
+	 * @return area double devuelve un número tipo double
+	 */
 	@Override
 	public double area() {
 		return l1 * l2;
 	}
-	
+
+	/**
+	 * Método que permite calcular el perímetro
+	 * @return perimetro double devuelve un número tipo double
+	 */
 	@Override
 	public double perimetro() {
 		return (2 * l1) + (2 * l2);
