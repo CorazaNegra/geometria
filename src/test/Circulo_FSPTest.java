521bd5e694c1ac5497c715f3404c36e4f65ce8dc
Francisco Salar Pérez <frasalper@alu.edu.gva.es>
2023-02-22 21:53:16 +0100

    Uff ahora sí

---
+++ src/test/Circulo_FSPTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Circulo_FSPTest {
+
+    @org.junit.jupiter.api.Test
+    void area() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void perimetro() {
+    }
+}
