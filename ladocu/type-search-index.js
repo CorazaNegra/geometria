569cec9199d67868d60b9443d997dabecfffe9ac
Francisco Salar Pérez <frasalper@alu.edu.gva.es>
2023-02-22 10:46:17 +0100

    Documentar es de sabios, aunque leerla sea pesado

---
+++ ladocu/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Circulo_FSP"},{"p":"<Unnamed>","l":"FiguraGeometrica_FSP"},{"p":"<Unnamed>","l":"Rectangulo_FSP"},{"p":"<Unnamed>","l":"Triangulo_FSP"}];updateSearchResults();
